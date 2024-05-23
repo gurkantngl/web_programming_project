22890648e1ea8a9c9e7e2b4c84599af212e1d305
Gürkan Töngel <92158772+gurkantngl@users.noreply.github.com>
2024-05-23 18:51:17 +0300

    cloud service update

--- web/my_app/nlp-analysis.py
+++ web/my_app/nlp-analysis.py
@@ -1,11 +1,1 @@
-import os
-from google.cloud import language_v1
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-account-file.json"
-client = language_v1.LanguageServiceClient()
-
-text = "Google Cloud is great!"
-document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-response = client.analyze_sentiment(request={'document': document})
-print("Metnin Duygu Puanı: ", response.document_sentiment.score)
-print("Metnin Duygu Büyüklüğü: ", response.document_sentiment.magnitude)
--- web/web/presentation/views.py
+++ web/web/presentation/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+import os
+from google.cloud import language_v1
 
 def login_view(request):
     return render(request, 'login.html')
 
 def register_view(request):
     return render(request, 'register.html')
+
+
+def nlp_analysis(text):
+    # Hizmet hesabı anahtarınızın yolunu belirtin
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your-service-account-file.json"
+
+    # Natural Language API istemcisini oluşturun
+    client = language_v1.LanguageServiceClient()
+
+    # Metin belgesi oluşturun
+    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+    # Metin duygu analizini yapın
+    response = client.analyze_sentiment(request={'document': document})
+
+    string = "Metnin Duygu Puanı: ", response.document_sentiment.score
+    string += "Metnin Duygu Büyüklüğü: ", response.document_sentiment.magnitude
+
+    return string
+    

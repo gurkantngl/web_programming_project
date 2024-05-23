42299c5c9cdf38ac974d1ec00c8f783ca92a48f8
Eren Bilgin <erenblgn9@hotmail.com>
2024-05-23 18:13:28 +0300

    RBCA

---
+++ web/web/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .presentation import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+]

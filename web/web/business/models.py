a8bab9f348ff2f972fd809e7c602e7cc309ac7f5
Eren Bilgin <erenblgn9@hotmail.com>
2024-05-23 18:02:06 +0300

    RBCA

---
+++ web/web/business/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+class Permission(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    fullname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10)
+    birth_date = models.DateField(null=True, blank=True)
+    password = models.CharField(max_length=255)
+    roles = models.ManyToManyField(Role)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def has_permission(self, permission_name):
+        for role in self.roles.all():
+            if role.permissions.filter(name=permission_name).exists():
+                return True
+        return False
+
+    def __str__(self):
+        return self.fullname
